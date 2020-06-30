abb34e350094557fa71f363d338857eea02f4381
Htto Hu <huyuantao@outlook.com>
2020-06-30 10:52:10 +0800

    update LICENSE content of each file.

--- interpreter/source/error.cpp
+++ interpreter/source/error.cpp
@@ -1,6 +1,10 @@
 /*
-* MIT License
-* Copyright (c) 2019 Htto Hu
+GNU GENERAL PUBLIC LICENSE
+					   Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
 */
 #include "../include/error.hpp"
 #include "../include/lexer.hpp"
--- interpreter/source/parser.cpp
+++ interpreter/source/parser.cpp
@@ -1,6 +1,10 @@
-﻿/*function
-* MIT License
-* Copyright (c) 2019 Htto Hu
+﻿/*
+	 GNU GENERAL PUBLIC LICENSE
+						   Version 3, 29 June 2007
+
+	 Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+	 Everyone is permitted to copy and distribute verbatim copies
+	 of this license document, but changing it is not allowed.
 */
 #include "../include/parser.hpp"
 #include "../include/expr.hpp"
